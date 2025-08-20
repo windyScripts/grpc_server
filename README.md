6e56b570ef6cef85b7318aea69bdc79b05f8736c
Aravind <aravindshenoy@ideaclan.com>
2025-08-20 16:04:06 +0530

    Adjusts location of files, completes server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+protoc --go_out=. --go-grpc_out=. proto/main.proto
+
 go mod init <name>
 
 go get google.golang.org/grpc
--- server.go
+++ server.go
@@ -1,3 +1,43 @@
 package main
 
-func main(){}
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "simplegrpcserver/proto/gen"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCalculateServer
+}
+
+func (s *server)Add(ctx context.Context, req *pb.AddRequest)(*pb.AddResponse, error){
+	return &pb.AddResponse{
+		Sum: req.A + req.B,
+	},nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterCalculateServer(grpcServer, &server{})
+
+	// skipped
+
+	fmt.Println("Server started on 50051")
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("Failed to serve: ", err)
+	}
+
+}
